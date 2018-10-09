13b8f01406e0d93e54d35c90c8d8d0166feff314
Alberto Godinez <alberto.godinez90@gmail.com>
2018-10-09 12:48:38 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/MY_APP_NAME'));
+app.use(express.static(__dirname + '/dist/mobilityx-hackathon-frontend'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mobilityx-hackathon-frontend'));
 });
 
 // default Heroku port
